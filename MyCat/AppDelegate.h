2c969c18beec0f91ee13b35c67616825f24052dc
刘洋 <123758328@qq.com>
2017-10-13 16:37:27 +0800

    first commit

---
+++ MyCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCat
+//
+//  Created by 刘洋 on 2017/10/12.
+//  Copyright © 2017年 刘洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
